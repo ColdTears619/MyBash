690a675d5d9bd93b8e551f6b39fa524995289e35
AtlasV <83712348+ColdTears619@users.noreply.github.com>
2023-05-26 19:45:30 +0330

    Take backup from file and directorys
    in first step script will backup from your file's and directory's and make them compress next step script will upload compress file to your backup server.

---
+++ BackUp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BACKUP_DIR=/home/coldtears/Desktop/backup
+DATE=$(date +%Y-%m-%d_%H-%M-%S)
+BACKUP_FILE=backup_$DATE.tar.gz
+
+tar czf $BACKUP_DIR/$BACKUP_FILE 
+
+scp -P PORT $BACKUP_DIR/$BACKUP_FILE USR@IP:/home/ubuntu
